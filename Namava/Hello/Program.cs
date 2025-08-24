c9c4f6509ab49d9ff4a0a63837ddd7467ddd7891
sharmaann <shakerian.arman@yahoo.com>
2025-08-24 20:22:30 +0330

    Initial commit console the numbers themselves and not the indices

---
+++ Namava/Hello/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld{
+    class Program{
+        static void Main(string[] args){
+
+            int []nums = {1, 6, 12, 4, 1, 4};
+            int target = 8;
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i ++){
+                if (! map.ContainsKey(nums[i]))
+                    map[nums[i]] = 0;
+
+                map[nums[i]] ++;
+
+                int need = target - nums[i];
+                if (map.ContainsKey(need)){
+                    if (need == nums[i] && map[nums[i]] < 2) // A number shouldn't be paired with itself unless there is another occurance of it
+                        continue;
+                    Console.WriteLine($"{nums[i]}, {need}");
+                }
+            }
+        }
+    }
+
+}
