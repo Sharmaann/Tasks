8dc6b56e7f603756235c22198e0c30fe3cbec13d
ArmanShakerian <shakerian.arman123@gmail.com>
2025-11-14 22:15:00 +0330

    solve W6 P1

--- Namava/hacker_rank/Problems/Program.cs
+++ Namava/hacker_rank/Problems/Program.cs
@@ -6,7 +6,7 @@
 
         static void Main(string [] args)
         {
-            FullCountingSort.Run();
+            PrimeDates.Run();
         }
     }
 } 
---
+++ Namava/hacker_rank/Problems/W6/PrimeDates.cs
@@ -0,0 +1,8 @@
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-prime-date/problem
+
+using System;
+
+class PrimeDates
+{
+    public static void Run() { }
+}
